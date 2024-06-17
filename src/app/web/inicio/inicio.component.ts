3e13ceeb5159e96f3d0b2abb9b5f257bbdc1258c
Pedro <pedro9122002@gmail.com>
2024-06-17 12:55:06 +0200

    final de la app.

--- src/app/web/inicio/inicio.component.ts
+++ src/app/web/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
-  onLinkClick() {
-    console.log('Link clicked!');
+  onLinkClick(page: string) {
+    this.route.navigate([page]);
   }
 
 }
